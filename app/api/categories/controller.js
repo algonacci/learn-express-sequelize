08a50042dfdc6844bc6047fac82e7e59d4979a63
Zetta Eric Julianto <algonacci1@gmail.com>
2022-08-15 20:42:01 +0700

    feat: Create, Update, and Delete Categories

--- app/api/categories/controller.js
+++ app/api/categories/controller.js
@@ -17,5 +17,82 @@ module.exports = {
             });
             next(error);
         }
+    },
+
+    createCategories: async (req, res, next) => {
+        try {
+            const { name } = req.body;
+            const category = await Category.create({
+                name,
+                user: req.user.id
+            });
+            res.status(201).json({
+                message: "Successfully created category",
+                data: category
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            });
+            next(error);
+        }
+    },
+
+    updateCategories : async (req, res, next) => {
+        try {
+            const { name } = req.body;
+            const { id } = req.params;
+            const category = await Category.findOne({
+                where: {
+                    id,
+                    user: req.user.id
+                }
+            });
+            if (!category) {
+                res.status(404).json({
+                    message: "Category not found"
+                });
+            } else {
+                await category.update({
+                    name
+                });
+                res.status(200).json({
+                    message: "Successfully updated category",
+                    data: category
+                });
+            }
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            });
+            next(error);
+        }
+    },
+
+    deleteCategories : async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const category = await Category.findOne({
+                where: {
+                    id,
+                    user: req.user.id
+                }
+            });
+            if (!category) {
+                res.status(404).json({
+                    message: "Category not found"
+                });
+            } else {
+                await category.destroy();
+                res.status(200).json({
+                    message: "Successfully deleted category"
+                });
+            }
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            });
+            next(error);
+        }
     }
 }
--- app/api/categories/router.js
+++ app/api/categories/router.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../middlewares/auth');
-const { getAllCategories } = require('./controller');
+const { getAllCategories, createCategories, updateCategories, deleteCategories } = require('./controller');
 
 router.get('/categories', auth, getAllCategories);
+router.post('/categories', auth, createCategories);
+router.put('/categories/:id', auth, updateCategories);
+router.delete('/categories/:id', auth, deleteCategories);
 
 module.exports = router;
